19fe535818937ad8a95d38efb4f34e901dd6c51d
grahamna <nate.a.graham@gmail.com>
2024-05-21 19:31:41 -0700

    pause, seek left/right
    Enabled hotkeys seek word prev / next using the arrow keys. This required switching from a for loop to a while loop

--- speedRead/main.py
+++ speedRead/main.py
@@ -3,31 +3,60 @@ import os
 import re
 import keyboard
 
+
+def wordNav(direction:int):
+    global going, count
+    if going: return
+    else:
+        if direction ==  1:
+            count+=1
+            terminalPrint(count)
+        elif direction == -1:
+            count-=1
+            terminalPrint(count)
+
+
 def speedMod(mod:int):
-    global speedVar
+    global speedVar, going
     if mod == 0:
         speedVar = speedVar + 0.05
+        if (not going):
+            terminalPrint(count)
     elif mod == 1:
         speedVar = speedVar - 0.05
+        if (not going):
+            terminalPrint(count)
     else:
-        global going
         going = not going
         
-def printToTerminal(inString:str, fileName:str):
+def printToTerminal(fileName:str):
+    
+    global count, speedVar, going, neededWhiteSpace, oldTerminalWidth, length, inString
+
     length = len(inString)
-    count = 0
+
     speedText = "lower is faster"
     progressText = "current / total"
-    oldTerminalWidth:int = os.get_terminal_size()[0]
+    oldTerminalWidth = os.get_terminal_size()[0]
     neededWhiteSpace = oldTerminalWidth - len(speedText) - len(progressText)
     print(f'\r{progressText}{fileName.center(neededWhiteSpace -1 , " " )}{speedText}')
-    for word in inString:
-        global going
+
+    while count != length:
         if (not going):
-            keyboard.wait("space")
-        global speedVar
-        count = count + 1
-        progressString:str = f'{count}/{length}'
+            keyboard.wait('space')
+        speed, count = terminalPrint(count)
+        if 0.1 + speed > 0:
+            time.sleep(0.1 + speed)
+            count+=1
+        else: 
+            speed = 0.1
+            speedVar = 0
+
+def terminalPrint(newCount) -> (int, int):
+        global oldTerminalWidth, neededWhiteSpace, count, speedVar, inString
+        count = newCount
+        word = inString[count]
+        progressString:str = f'{count + 1}/{length}'
         newWord:str = f"> {word} <"
         terminalWidth:int = os.get_terminal_size()[0]
         if (oldTerminalWidth < terminalWidth):
@@ -38,9 +67,9 @@ def printToTerminal(inString:str, fileName:str):
             print('\033[2K\033[1G\033[F\033[2K\033[1G',end='',flush=True)
         neededWhiteSpace = terminalWidth - (len(progressString) + len(f'Speed:{speedVar:.2f}')) - 8
         wordSize = len(word)
-        speed = (wordSize*0.018) * speedVar 
+        speed = (wordSize*0.02) * speedVar 
         print(f'\r{progressString}{newWord.center(neededWhiteSpace, " " )}Speed:{speedVar:.2f}', end='')
-        time.sleep(0.1 + speed)
+        return speed, count
 
 def splitUtilString(strIn) -> str:
     if(strIn == ''): 
@@ -48,7 +77,7 @@ def splitUtilString(strIn) -> str:
     if(isinstance(strIn, str)):
         temp = strIn.strip()
         if temp != '':
-            res = re.split('[\s\n\t]+', temp)
+            res = re.split(r'\s+', temp)
             return res
         return
     else:
@@ -60,23 +89,30 @@ def splitUtilString(strIn) -> str:
 
 def main():
     
-    global speedVar, going
+    global speedVar, going, count, inString
+    count = 0
     speedVar = 1
     going = False
-    
+
     fin = input("Name of file in the folder /input \n => ")
     
+    keyboard.add_hotkey("LEFT", lambda: wordNav(-1))
+    keyboard.add_hotkey("RIGHT", lambda: wordNav(1))
     keyboard.add_hotkey("DOWN", lambda: speedMod(1))
     keyboard.add_hotkey("UP", lambda: speedMod(0))
     keyboard.add_hotkey("space", lambda: speedMod(-1))
     
     dirname = os.path.dirname(__file__)
-    myFile = os.path.join(dirname, f'../input/{fin}')
+    myFile:str = os.path.join(dirname, f'../input/{fin}')
     with open(myFile, 'r', encoding="utf-8") as inputFile:
         buffer = inputFile.read()
         
-        res = splitUtilString(buffer)
-        printToTerminal(res, fin)
+        inString = splitUtilString(buffer)
+    
+    try:
+        printToTerminal(fin)
+    except KeyboardInterrupt:
+        print("\nExiting via keyboardInterrupt")
     
     # test1 = "this is a \ntest for a \tstring"
     # test2 = ["testing","for","an","array","of","strings with some variance             ","\t\\t \c\r","123\n321"]
